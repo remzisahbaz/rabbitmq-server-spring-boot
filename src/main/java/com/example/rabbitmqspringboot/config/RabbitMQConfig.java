038234fc136d3e2a4e85666296b4abfd874d378b
remzisahbaz <remzisahbaz@outlook.com>
2022-12-24 13:40:19 +0300

    rabbitmq -config

--- src/main/java/com/example/rabbitmqspringboot/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbitmqspringboot/config/RabbitMQConfig.java
@@ -1,7 +1,14 @@
 package com.example.rabbitmqspringboot.config;
 
-import org.springframework.amqp.core.Queue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,13 +23,52 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${queue}")
+    @Value("${spring.rabbitmq.queue.json.fromQueue}")
     private String queue;
+    @Value("${spring.rabbitmq.key.toKey}")
+    private String key;
 
-    private Queue queue(){
-        return new Queue("xx",true);
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Bean
+    public Queue toQueue(){
+        return new Queue(queue,true);
     }
 
 
+    @Bean
+    public TopicExchange topicExchange(){
+        return new TopicExchange("exc");
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(toQueue())
+                .to(topicExchange())
+                .with("key");
+
+    }
+
+   @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMandatory(true);
+        return rabbitTemplate;
+
+   }
+
+   @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter(objectMapper);
+   }
+
+   @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+   }
 }
 
