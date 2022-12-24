45ed20091b699301027f65b32d42cf4c183ff601
remzisahbaz <remzisahbaz@outlook.com>
2022-12-24 12:39:51 +0300

    committed

--- src/main/java/com/example/rabbitmqspringboot/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbitmqspringboot/config/RabbitMQConfig.java
@@ -1,10 +1,28 @@
-package com.example.rabbitmqspringboot.config;/**
-    *@Project
-     *@Created by Author (remzisahbaz)
-        *@Date (24.12.2022)
-*
-**/
+package com.example.rabbitmqspringboot.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
 
 
+
+/**
+ * @Project
+ * @Created by Author (remzisahbaz)
+ * @Date (24.12.2022)
+ **/
+
+@Configuration
 public class RabbitMQConfig {
+
+    @Value("${queue}")
+    private String queue;
+
+    private Queue queue(){
+        return new Queue("xx",true);
+    }
+
+
 }
+
--- src/main/java/com/example/rabbitmqspringboot/model/Message.java
+++ src/main/java/com/example/rabbitmqspringboot/model/Message.java
@@ -1,10 +1,26 @@
-package com.example.rabbitmqspringboot.model;/**
-    *@Project
-     *@Created by Author (remzisahbaz)
-        *@Date (24.12.2022)
-*
-**/
+package com.example.rabbitmqspringboot.model;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+/**
+ * @Project
+ * @Created by Author (remzisahbaz)
+ * @Date (24.12.2022)
+ **/
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+
+    private String messageId;
+    private String from;
+    private String to;
+    private String messageText;
+
 }
